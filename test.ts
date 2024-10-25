6f9347ca4d059fc38c79b3db4f6bcc5214c68ce0
RusiriniWijethunga <95708918+RusiriniWijethunga@users.noreply.github.com>
2024-10-26 01:52:19 +0530

    changed the js and ts files

--- test.ts
+++ test.ts
@@ -1,9 +1,23 @@
 figma.showUI(__html__);
 
 figma.ui.onmessage = msg =>{
-    if(msg.type === 'applyOpacity'){
-
-    }else if(msg.type === 'exitPlugin'){
+    if(msg.type === 'createRectangle'){
+        const rect = figma.createRectangle();
+        rect.resize(150,150);
+        rect.fills = [{type:'SOLID',color:{r:0,g:0,b:0}}];
+        rect.cornerRadius=4;
+        figma.currentPage.appendChild(rect);
+    }
+    else if(msg.type==='createEllipse'){
+        if(msg.type === 'createEllipse'){
+            const ellipse = figma.createEllipse();
+            ellipse.resize(150,150);
+            ellipse.fills = [{type:'SOLID',color:{r:0,g:0,b:0}}];
+            //ellipse.cornerRadius=4;
+            figma.currentPage.appendChild(ellipse);
+    }
+    else if(msg.type === 'exitPlugin'){
         figma.closePlugin();
     }
+}
 }
