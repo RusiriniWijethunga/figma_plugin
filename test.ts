42d9f616ec1774341d82b66c3d414e399898ada1
RusiriniWijethunga <95708918+RusiriniWijethunga@users.noreply.github.com>
2024-10-27 00:12:05 +0530

    Update code file with new changes

--- test.ts
+++ test.ts
@@ -1,21 +1,24 @@
 figma.showUI(__html__);
 
 figma.ui.onmessage = msg =>{
-    if(msg.type === 'createRectangle'){
-        const rect = figma.createRectangle();
-        rect.resize(150,150);
-        rect.fills = [{type:'SOLID',color:{r:0,g:0,b:0}}];
-        rect.cornerRadius=4;
-        figma.currentPage.appendChild(rect);
-    }
-    else if(msg.type==='createEllipse'){
-        if(msg.type === 'createEllipse'){
-            const ellipse = figma.createEllipse();
-            ellipse.resize(150,150);
-            ellipse.fills = [{type:'SOLID',color:{r:0,g:0,b:0}}];
-            //ellipse.cornerRadius=4;
-            figma.currentPage.appendChild(ellipse);
-    }
+    if(msg.type === 'runPlugin'){
+    //     const rect = figma.createRectangle();
+    //     rect.resize(150,150);
+    //     rect.fills = [{type:'SOLID',color:{r:0,g:0,b:0}}];
+    //     rect.cornerRadius=4;
+    //     figma.currentPage.appendChild(rect);
+    // }
+    for (const node of figma.currentPage.selection{
+        console.log.(node.type);
+    })
+    // else if(msg.type==='createEllipse'){
+    //     if(msg.type === 'createEllipse'){
+    //         const ellipse = figma.createEllipse();
+    //         ellipse.resize(150,150);
+    //         ellipse.fills = [{type:'SOLID',color:{r:0,g:0,b:0}}];
+    //         //ellipse.cornerRadius=4;
+    //         figma.currentPage.appendChild(ellipse);
+    // }
     else if(msg.type === 'exitPlugin'){
         figma.closePlugin();
     }
